012a8e25bd3e8536e548ac522bc42d5df85e3c85
Xigaz <Xigaz1@gmail.com>
2018-04-19 23:08:44 -0600

    initial Commit

--- src/herm/Player.java
+++ src/herm/Player.java
@@ -1,9 +1,25 @@
 package herm;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Player
 {
+	String[] nameOptions = {
+			"Phineas",
+			"Ferb",
+			"Dr. Doof",
+			"Candace",
+			"Perry",
+			"Baljeet",
+			"Carl",
+			"Isabella",
+			"Buford",
+			"Jeremy",
+			"Stacy",
+			"Vanessa"
+	};
+
 	private String name;
 	private ArrayList<Card> hand;
 	private Notebook myNotebook;
@@ -11,6 +27,12 @@ public class Player
 	public Player (String n, ArrayList<Card> h)
 	{
 		name = n;
+		hand = h;
+	}
+
+	public Player (ArrayList<Card> h)
+	{
+		name = nameOptions[(new Random()).nextInt(nameOptions.length)];
 		hand = h;
 	}
 
