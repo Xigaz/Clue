65d9139a9bc9a84eb56f7798caaadd93f2311994
Xigaz <Xigaz1@gmail.com>
2018-04-23 14:40:47 -0600

    initial Commit

--- src/herm/Room.java
+++ src/herm/Room.java
@@ -29,7 +29,7 @@ public enum Room implements CardOptions {
 
 	public Room getPassageExit()
 	{
-		return valueOf(passageExit);
+		return valueOf(passageExit.toUpperCase());
 	}
 
 	public String toString()
